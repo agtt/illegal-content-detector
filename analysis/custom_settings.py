9f19a385fa579c0dcb0aa1b5bcd0d875583d41df
Agit <csd@hotmail.com.tr>
2019-09-27 03:15:04 +0300

    celery

--- analysis/custom_settings.py
+++ analysis/custom_settings.py
@@ -5,6 +5,10 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 # CELERY SETTING
 import djcelery
 djcelery.setup_loader()
+djcelery.setup_loader()
+CELERY_TIMEZONE = 'Europe/Istanbul'
+# CELERY_IMPORTS = ('erp.tasks',)
+CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 CELERY_BROKER_URL = 'amqp://localhost'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'django-cache'
