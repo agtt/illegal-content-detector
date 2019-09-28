127126c706e2b9882ff0e88d61edfd02620b78cf
Agit <csd@hotmail.com.tr>
2019-09-28 16:15:22 +0300

    Crawler Class

--- apps/reports/libs/crawler/__init__.py
+++ apps/reports/libs/crawler/__init__.py
@@ -1,3 +1,14 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+
 class Crawler:
-    def __init__(self):
-        pass
+    def __init__(self,url):
+        self.url = url
+
+    def source(self):
+        r = requests.get(self.url)
+        soup = BeautifulSoup(r.text, "html.parser")
+        return soup
